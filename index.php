72f3af946f659ed1e09bed632160b791e55ab041
João Vítor <joaovitorsf7@hotmail.com>
2020-11-04 16:38:10 -0300

    atributos do celular.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+class Celular {
+    private $cor;
+
+    private $marca;
+    private $modelo;
+
+    private $qtdeMemoriaArmazenamento;
+    private $qtdeMemoriaRAM;
+
+    private $sistemaOperacional;
+}
