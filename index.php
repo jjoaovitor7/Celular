2eda70872c1370254f615bd347f4dbb8e515f318
João Vítor <joaovitorsf7@hotmail.com>
2020-11-04 16:51:34 -0300

    setters.

--- index.php
+++ index.php
@@ -9,4 +9,29 @@ class Celular {
     private $qtdeMemoriaRAM;
 
     private $sistemaOperacional;
+
+    public function setCor($cor){
+        $this->$cor = $cor;
+    }
+
+    public function setMarca($marca){
+        $this->$marca = $marca;
+    }
+
+    public function setModelo($modelo){
+        $this->modelo = $modelo;
+    }
+
+    public function setQtdeMemoriaArmazenamento($qtdeMemoriaArmazenamento){
+        $this->$qtdeMemoriaArmazenamento = $qtdeMemoriaArmazenamento;
+    }
+
+    public function setQtdeMemoriaRAM($qtdeMemoriaRAM){
+        $this->$qtdeMemoriaRAM = $qtdeMemoriaRAM;
+    }
+
+    public function setSO($so){
+        $this->sistemaOperacional = $so;
+    }
+
 }
