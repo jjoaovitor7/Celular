ed1ebe4b10ce48e40bb051bb652465221350da57
João Vítor <joaovitorsf7@hotmail.com>
2020-11-06 18:33:35 -0300

    ContatoTest.

---
+++ tests/ContatoTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once(getcwd()."/src/Contato.php");
+
+
+final class ContatoTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $contato = new Contato();
+        $this->assertEquals(0, intval(count($contato->getContatoArray())));
+        $array = array("nome" => "Teste", "numero" => "(00)00000-0000");
+        $contato->addContato($array);
+        $this->assertEquals(1, intval(count($contato->getContatoArray())));
+    }
+}
