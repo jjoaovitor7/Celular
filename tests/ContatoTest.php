ab9b2c63cc704195220d7e66ac4e2021f019fa1d
João Vítor <joaovitorsf7@hotmail.com>
2020-11-06 19:12:02 -0300

    testAddContato e testDeleteContato.

--- tests/ContatoTest.php
+++ tests/ContatoTest.php
@@ -7,12 +7,23 @@ require_once(getcwd()."/src/Contato.php");
 
 final class ContatoTest extends TestCase
 {
-    public function testPushAndPop()
+    public function testAddContato()
     {
         $contato = new Contato();
         $this->assertEquals(0, intval(count($contato->getContatoArray())));
         $array = array("nome" => "Teste", "numero" => "(00)00000-0000");
         $contato->addContato($array);
         $this->assertEquals(1, intval(count($contato->getContatoArray())));
+    }
+
+    public function testDeleteContato()
+    {
+        $contato = new Contato();
+        $this->assertEquals(0, intval(count($contato->getContatoArray())));
+        $array = array("nome" => "Teste", "numero" => "(00)00000-0000");
+        $contato->addContato($array);
+        $this->assertEquals(1, intval(count($contato->getContatoArray())));
+        $contato->deletarContato(0);
+        $this->assertEquals(0, intval(count($contato->getContatoArray())));
     }
 }
