f72cce3f076d40b5c68b4251cc0809c5641e70c6
João Vítor <joaovitorsf7@hotmail.com>
2020-11-11 00:09:27 -0300

    refatoração, if p/ switch, porém com bug.

--- src/main.php
+++ src/main.php
@@ -27,137 +27,132 @@ if (strtok($ligarCelular, "\n") == "S" || strtok($ligarCelular, "\n") == "s")
     {
         $showOptionsCond = fgets(STDIN);
 
-
-        // Home
-        if ($showOptionsCond == 1)
-        {
-            clearScreen();
-            $celular->showOptionsHome();
-        }
-
-
-        // Contatos
-        else if ($showOptionsCond == 2)
-        {
-            clearScreen();
-            $celular->showOptionsContatos();
-            $contatosOptions = fgets(STDIN);
-            if ($contatosOptions == 1)
-            {
+        switch ($showOptionsCond) {
+            // Home
+            case 1:
                 clearScreen();
-                $option->addContact($contato);
                 $celular->showOptionsHome();
-            }
-            
-            else if ($contatosOptions == 2)
+            break;
+
+            // Contatos
+            case 2:
+                clearScreen();
+                $celular->showOptionsContatos();
+                $contatosOptions = fgets(STDIN);
+
+            switch ($contatosOptions)
             {
-                echo $contato->getContato();
-                echo "\n";
-                $celular->showOptionsHome();
-            }
-
-            else if ($contatosOptions == 3)
-            {
-                $option->selectContact($celular, $contato);
-            }
-
-            else if ($contatosOptions == 4)
-            {
-                $option->deleteContact($celular, $contato);
-            }
-            else {
-                echo "Opção não encontrada.\n";
-                $celular->showOptionsHome();
-            }
-        }
-
-
-        // Enviar mensagem
-        else if ($showOptionsCond == 3)
-        {
-            $option->sendMessage($celular, $contato);
-        }
-
-        else if ($showOptionsCond == 4)
-        {
-            clearScreen();
-            echo "Navegadores";
-            echo "\n1-Firefox";
-            echo "\n2-Chrome";
-            echo "\n:";
-            $acessarInternet = fgets(STDIN);
-            if ($acessarInternet == 1)
-            {
-                echo "Abrindo Firefox...\n";
-                popen("firefox -url https://google.com", "r");
-                $celular->showOptionsHome();
-            }
-
-            else if ($acessarInternet == 2)
-            {
-                echo "Abrindo Chrome...\n";
-                popen("google-chrome -url https://google.com", "r");
-                sleep(3);
-                $celular->showOptionsHome();
-            }
-
-            else {
-                echo "Navegador não encontrado.";
-            }
-        }
-
-        // Configurações
-        else if ($showOptionsCond == 5)
-        {
-            clearScreen();
-            if ($celular->getStatusProprietario() == false)
-            {
-                $celular->showOptionsConfig();
-                $configOptions = fgets(STDIN);
-                $option->configWithAboutAndWithoutOwnerDefined($celular, false, $configOptions);
-
-                if (intval($configOptions) <= 0 ||intval($configOptions) >= 3 || gettype(intval($configOptions)) != "integer"){
-                    echo "Opção não encontrada.\n";
-                    $celular->showOptionsHome();
-                }
-            } else {
-                $celular->showOptionsConfigWithProprietario();
-                $configOptions = fgets(STDIN);
-        
-                $option->configWithAboutAndWithoutOwnerDefined($celular, true, $configOptions);
-                if ($configOptions == 3)
-                {
+                case 1:
                     clearScreen();
-                    echo "Nome do Proprietário: ";
-                    echo $celular->getNomeProprietario();
+                    $option->addContact($contato);
                     $celular->showOptionsHome();
-                }
+                break;
 
-                if (intval($configOptions) <= 0 ||intval($configOptions) >= 4 || gettype(intval($configOptions)) != "integer"){
+                case 2:
+                    echo $contato->getContato();
+                    echo "\n";
+                    $celular->showOptionsHome();
+                break;
+
+                case 3:
+                    $option->selectContact($celular, $contato);
+                break;
+
+                case 4:
+                    $option->deleteContact($celular, $contato);
+                break;
+
+                default:
                     echo "Opção não encontrada.\n";
                     $celular->showOptionsHome();
+                break;
+            }
+
+            break;
+
+            // Enviar mensagem
+            case 3:
+                $option->sendMessage($celular, $contato);
+            break;
+
+            case 4:
+                clearScreen();
+                echo "Navegadores";
+                echo "\n1-Firefox";
+                echo "\n2-Chrome";
+                echo "\n:";
+
+                $acessarInternet = fgets(STDIN);
+                switch ($acessarInternet)
+                {
+                    case 1:
+                        echo "Abrindo Firefox...\n";
+                        popen("firefox -url https://google.com", "r");
+                        $celular->showOptionsHome();
+                    break;
+
+                    case 2:
+                        echo "Abrindo Chrome...\n";
+                        popen("google-chrome -url https://google.com", "r");
+                        sleep(3);
+                        $celular->showOptionsHome();
+                    break;
+
+                    default:
+                        echo "Navegador não encontrado.";
+                    break;
                 }
 
-            }
-        }
+            // Configurações
+            case 5:
+                clearScreen();
+                if ($celular->getStatusProprietario() == false)
+                {
+                    $celular->showOptionsConfig();
+                    $configOptions = fgets(STDIN);
+                    $option->configWithAboutAndWithoutOwnerDefined($celular, false, $configOptions);
 
+                    if (intval($configOptions) <= 0 ||intval($configOptions) >= 3 || gettype(intval($configOptions)) != "integer"){
+                       echo "Opção não encontrada.\n";
+                        $celular->showOptionsHome();
+                    }
+                } else {
+                    $celular->showOptionsConfigWithProprietario();
+                    $configOptions = fgets(STDIN);
+        
+                    $option->configWithAboutAndWithoutOwnerDefined($celular, true, $configOptions);
+                    if ($configOptions == 3)
+                    {
+                        clearScreen();
+                        echo "Nome do Proprietário: ";
+                        echo $celular->getNomeProprietario();
+                        $celular->showOptionsHome();
+                    }
 
-        // Desligar
-        else if ($showOptionsCond == 6)
-        {
-            $celular->desligar();
-            echo "\n";
-            $isTrue = false;
+                    if (intval($configOptions) <= 0 ||intval($configOptions) >= 4 || gettype(intval($configOptions)) != "integer"){
+                        echo "Opção não encontrada.\n";
+                        $celular->showOptionsHome();
+                    }
+                }
+            break;
+
+            // Desligar
+            case 6:
+                $celular->desligar();
+                echo "\n";
+                $isTrue = false;
+            break;
+
+            default:
+                echo "Opção não encontrada.\n";
+                echo "Estamos te redirecionando para a Home.\n";
+                sleep(5);
+                clearScreen();
+                $celular->showOptionsHome();
             break;
         }
-        else {
-            echo "Opção não encontrada.\n";
-            echo "Estamos te redirecionando para a Home.\n";
-            sleep(5);
-            clearScreen();
-            $celular->showOptionsHome();
-        }
     }
-} else {
+}
+else {
     echo "O celular não será ligado.\n";
 }
